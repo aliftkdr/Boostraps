616d04306a99c35f5de94bb5e41d8e6b2c204360
aliftkdr <alifalfi262@gmail.com>
2024-10-02 08:49:20 +0800

    Add files via upload
    Signed-off-by: aliftkdr <alifalfi262@gmail.com>

---
+++ js.js
@@ -0,0 +1,4 @@
+
+document.getElementById('contactButton').addEventListener('click', function() {
+    alert('Terima kasih sudah mengunjungi halaman ini! Silakan hubungi saya melalui email: alifalfi262@gmail.com');
+});
